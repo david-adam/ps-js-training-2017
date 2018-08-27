2408e2f02431a4ea3f3c70b3349ec459b1bacd0b
David/Adam Dai <wdai1@paypalcorp.com>
2018-08-27 17:15:36 +0800

    customize to my own settings and testing values

--- src/routes/paypal/classic/setexpresscheckout/ex1.js
+++ src/routes/paypal/classic/setexpresscheckout/ex1.js
@@ -7,7 +7,17 @@ export default function (router) {
         let nvpParams = {
             RETURNURL: 'https://' + c9config.workspace + '-' + c9config.username + '.c9users.io/misc/returnurl.html',
             CANCELURL: 'https://' + c9config.workspace + '-' + c9config.username + '.c9users.io/misc/cancelurl.html',
-            PAYMENTREQUEST_0_AMT: '100.00'
+            PAYMENTREQUEST_0_AMT: '50.00',
+            PAYMENTREQUEST_0_SHIPTONAME: 'Piyachat Saichan',
+            PAYMENTREQUEST_0_SHIPTOSTREET: 'Lumpini Place 1901-4004/566',
+            PAYMENTREQUEST_0_SHIPTOSTREET2: 'Rama 4 Kluaynamthai, Khlong Toei',
+            PAYMENTREQUEST_0_SHIPTOCITY: 'Bangkok',
+            PAYMENTREQUEST_0_SHIPTOSTATE: 'Bangkok',
+            PAYMENTREQUEST_0_SHIPTOZIP: '10110',
+            PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE: 'TH',
+            PAYMENTREQUEST_0_CURRENCYCODE: 'USD',
+            ADDROVERRIDE: "1",
+            NOSHIPPING: "0"
         };
             
         paypal.call('SetExpressCheckout', nvpParams, (err, response) => {
--- src/routes/paypal/classic/setexpresscheckout/index.js
+++ src/routes/paypal/classic/setexpresscheckout/index.js
@@ -7,7 +7,24 @@ export default function (router) {
         let nvpParams = {
             RETURNURL: 'https://' + c9config.workspace + '-' + c9config.username + '.c9users.io/misc/returnurl.html',
             CANCELURL: 'https://' + c9config.workspace + '-' + c9config.username + '.c9users.io/misc/cancelurl.html',
-            PAYMENTREQUEST_0_AMT: '100.00'
+            PAYMENTREQUEST_0_AMT: '50.00',
+            // PAYMENTREQUEST_0_SHIPTONAME: 'Piyachat Saichan',
+            // PAYMENTREQUEST_0_SHIPTOSTREET: 'P.O. Box 887',
+            // PAYMENTREQUEST_0_SHIPTOSTREET2: '2508 Dolor. Av.',
+            // PAYMENTREQUEST_0_SHIPTOCITY: 'Muskegon',
+            // PAYMENTREQUEST_0_SHIPTOSTATE: 'KY',
+            // PAYMENTREQUEST_0_SHIPTOZIP: '12482',
+            // PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE: 'US',
+            PAYMENTREQUEST_0_SHIPTONAME: 'Piyachat Saichan',
+            PAYMENTREQUEST_0_SHIPTOSTREET: '368 S Main St Rear',
+            //PAYMENTREQUEST_0_SHIPTOSTREET2: '2508 Dolor. Av.',
+            PAYMENTREQUEST_0_SHIPTOCITY: 'Andover',
+            PAYMENTREQUEST_0_SHIPTOSTATE: 'MA',
+            PAYMENTREQUEST_0_SHIPTOZIP: '01810-4824',
+            PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE: 'US',
+            PAYMENTREQUEST_0_CURRENCYCODE: 'USD',
+            ADDROVERRIDE: "1",
+            NOSHIPPING: "0"
         };
             
         paypal.call('SetExpressCheckout', nvpParams, (err, response) => {
