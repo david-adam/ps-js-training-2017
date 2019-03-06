8d2d6c1908bd34032529594352921dd3773efd19
David Dai <wdai1@paypal.com>
2019-03-06 17:29:48 +0800

    send string in callback

--- src/routes/paypal/classic/instantupdate/index.js
+++ src/routes/paypal/classic/instantupdate/index.js
@@ -1,26 +1,28 @@
 export default function(router) {
   router.post("/", (req, res) => {
-    let payload = {};
+    let payload = "";
     if (req.body.SHIPTOSTATE == "CA" || req.body.SHIPTOSTATE == "CALIFORNIA") {
-      payload = {
-        METHOD: "CallbackResponse",
-        L_SHIPPINGOPTIONNAME2: "Shipping Option 2 NON-default",
-        L_SHIPPINGOPTIONAMOUNT2: 10,
-        L_SHIPPINGOPTIONISDEFAULT2: true,
-        L_SHIPPINGOPTIONNAME3: "Shipping Option 3 NON-default",
-        L_SHIPPINGOPTIONAMOUNT3: 50,
-        L_SHIPPINGOPTIONISDEFAULT3: false
-      };
+      payload = [
+        "METHOD=CallbackResponse",
+        "L_SHIPPINGOPTIONNAME2=Shipping Option 2 NON-default",
+        "L_SHIPPINGOPTIONAMOUNT2=10",
+        "L_SHIPPINGOPTIONISDEFAULT2=true",
+        "L_SHIPPINGOPTIONNAME3=Shipping Option 3 NON-default",
+        "L_SHIPPINGOPTIONAMOUNT3=50",
+        "L_SHIPPINGOPTIONISDEFAULT3=false"
+      ].join("&");
     }
 
-    payload = {
-      L_SHIPPINGOPTIONISDEFAULT0: true,
-      L_SHIPPINGOPTIONNAME0: "Shipping Option 0 default",
-      L_SHIPPINGOPTIONAMOUNT0: 50,
-      L_SHIPPINGOPTIONISDEFAULT1: false,
-      L_SHIPPINGOPTIONNAME1: "Shipping Option 1 NON-default",
-      L_SHIPPINGOPTIONAMOUNT1: 60
-    };
-    res.json(payload);
+    payload = [
+      "METHOD=CallbackResponse",
+      "L_SHIPPINGOPTIONNAME0=Shipping Option 0 default",
+      "L_SHIPPINGOPTIONAMOUNT0=50",
+      "L_SHIPPINGOPTIONISDEFAULT0=true",
+      "L_SHIPPINGOPTIONNAME1=Shipping Option 1 NON-default",
+      "L_SHIPPINGOPTIONAMOUNT1=60",
+      "L_SHIPPINGOPTIONISDEFAULT1=false"
+    ].join("&");
+
+    res.send(payload);
   });
 }
