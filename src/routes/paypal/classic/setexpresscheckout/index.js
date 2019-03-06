f1629b49db13d4aab2bad129ed015defc8e7deeb
David Dai <wdai1@paypal.com>
2019-03-06 17:52:05 +0800

    forgot to specify the default shipping

--- src/routes/paypal/classic/setexpresscheckout/index.js
+++ src/routes/paypal/classic/setexpresscheckout/index.js
@@ -26,7 +26,7 @@ export default function(router) {
       PAYMENTREQUEST_0_SHIPPINGAMT: 0,
       CALLBACK: instantUpdateURL,
       CALLBACKTIMEOUT: callbackTimeout,
-      L_SHIPPINGOPTIONISDEFAULT0: false,
+      L_SHIPPINGOPTIONISDEFAULT0: true,
       L_SHIPPINGOPTIONNAME0: "Shipping Option normal default",
       L_SHIPPINGOPTIONAMOUNT0: 0,
       L_SHIPPINGOPTIONISDEFAULT1: false,
