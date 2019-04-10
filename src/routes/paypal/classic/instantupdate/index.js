f443b247b20a2243ca823bbe760a2f21e656888e
David Dai <wdai1@paypal.com>
2019-04-10 15:27:17 +0800

    add insurance mandatory in the instant update

--- src/routes/paypal/classic/instantupdate/index.js
+++ src/routes/paypal/classic/instantupdate/index.js
@@ -4,8 +4,10 @@ export default function(router) {
     if (req.body.SHIPTOSTATE == "CA" || req.body.SHIPTOSTATE == "CALIFORNIA") {
       payload = [
         "METHOD=CallbackResponse",
+        "OFFERINSURANCEOPTION=false",
         "L_SHIPPINGOPTIONNAME0=Shipping Option expensive NON-default",
         "L_SHIPPINGOPTIONAMOUNT0=20",
+        "L_INSURANCEAMOUNT0=3.0",
         "L_SHIPPINGOPTIONISDEFAULT0=true"
       ].join("&");
     }
