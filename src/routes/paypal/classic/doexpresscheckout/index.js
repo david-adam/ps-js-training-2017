70dcd1b7072c7cd2c83cac47989159106a1553f9
David Dai <wdai1@paypal.com>
2019-07-12 13:58:57 +0800

    fix DoEC AMT param

--- src/routes/paypal/classic/doexpresscheckout/index.js
+++ src/routes/paypal/classic/doexpresscheckout/index.js
@@ -5,16 +5,24 @@ export default function(router) {
     let nvpParams = {
       TOKEN: req.body.TOKEN,
       PAYERID: req.body.PAYERID,
-      AMT: req.body.AMT,
       VERSION: '124',
-      PAYMENTREQUEST_0_PAYMENTACTION: 'SALE',
-      PAYMENTREQUEST_0_SHIPTONAME: 'Piyachat Saichan',
-      PAYMENTREQUEST_0_SHIPTOSTREET: '368 S Main St Rear',
-      PAYMENTREQUEST_0_SHIPTOSTREET2: '2508 Dolor. Av.',
-      PAYMENTREQUEST_0_SHIPTOCITY: 'Andover',
-      PAYMENTREQUEST_0_SHIPTOSTATE: 'MA',
-      PAYMENTREQUEST_0_SHIPTOZIP: '01810-4824',
-      PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE: 'US'
+      SUBJECT: '',
+      PAYMENTREQUEST_0_AMT: req.body.AMT,
+      // PAYMENTREQUEST_0_TAXAMT: 0,
+      // PAYMENTREQUEST_0_SHIPPINGAMT: 0,
+      // PAYMENTREQUEST_0_CURRENCYCODE: 'USD',
+      // PAYMENTREQUEST_0_NOTIFYURL:
+      //   'https://94bd7ced.ngrok.io/pptest/ipn/listener.php',
+      // PAYMENTREQUEST_0_CUSTOM: 'PROMO',
+      // PAYMENTREQUEST_0_SHIPTOPHONENUM: '18665395108',
+      // PAYMENTREQUEST_0_PAYMENTACTION: 'SALE',
+      // PAYMENTREQUEST_0_SHIPTONAME: 'Piyachat Saichan',
+      // PAYMENTREQUEST_0_SHIPTOSTREET: '368 S Main St Rear',
+      // PAYMENTREQUEST_0_SHIPTOSTREET2: '2508 Dolor. Av.',
+      // PAYMENTREQUEST_0_SHIPTOCITY: 'Andover',
+      // PAYMENTREQUEST_0_SHIPTOSTATE: 'MA',
+      // PAYMENTREQUEST_0_SHIPTOZIP: '01810-4824',
+      // PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE: 'US'
     };
 
     paypal.call('DoExpressCheckoutPayment', nvpParams, (err, response) => {
